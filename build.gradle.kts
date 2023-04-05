a9f0ce04d6bc0ae64ef6c9138aeb96d3bac12620
i960107 <i960107@naver.com>
2023-04-05 18:14:34 +0900

    Init Project

---
+++ build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    id("org.springframework.boot") version "2.5.2"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.asciidoctor.jvm.convert") version "3.1.0"
+    id("java")
+}
+
+group = "com.naver.webnovel"
+version = "0.0.1-SNAPSHOT"
+
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+lateinit var asciidoctorExt: Configuration
+val snippetsDir = "build/generated-snippets"
+
+asciidoctorj {
+    asciidoctorExt = configurations.create("asciidoctorExt")
+}
+
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    //springboot
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    //mysql
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("mysql:mysql-connector-java") 
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    //lombok
+    implementation("org.projectlombok:lombok")
+
+    //test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    asciidoctorExt("org.springframework.restdocs:spring-restdocs-asciidoctor")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+
+tasks.test {
+    outputs.dir(snippetsDir)
+}
+
+tasks.asciidoctor {
+    dependsOn(tasks.test)
+    val snippets = file("build/generated-snippets")
+    configurations("asciidoctorExt")
+    attributes["snippets"] = snippets
+    inputs.dir(snippets)
+    sources { include("**/index.adoc") }
+    baseDirFollowsSourceFile()
+}
+
+tasks.bootJar {
+    dependsOn(tasks.asciidoctor)
+    from("${tasks.asciidoctor.get().outputDir}") {
+        into("static/docs")
+    }
+}
