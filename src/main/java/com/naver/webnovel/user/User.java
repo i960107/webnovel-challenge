9914e67cb8ab7878a087ddd84fbf026000f30feb
i960107 <i960107@naver.com>
2023-04-13 23:28:44 +0900

    Add Entity Classes

--- src/main/java/com/naver/webnovel/user/User.java
+++ src/main/java/com/naver/webnovel/user/User.java
@@ -1,45 +1,74 @@
 package com.naver.webnovel.user;
 
-import java.time.LocalDateTime;
+import com.naver.webnovel.base.BaseTimeEntity;
+import com.naver.webnovel.base.Gender;
+import com.naver.webnovel.base.Status;
+import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.validation.constraints.NotNull;
+import lombok.Builder;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.data.annotation.Id;
 
 @Entity
-public class User {
+public class User extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idx;
 
-    @Column(name = "id", nullable = false)
+    @NotNull
+    @Length(max = 20)
+    @Column(name = "id", length = 20, nullable = false)
     private String id;
 
-    @Column(name = "password", nullable = false)
+    @NotNull
+    @Length(max = 100)
+    @Column(name = "password", length = 100, nullable = false)
     private String password;
 
-    @Column(name = "nickname", nullable = false)
+    @NotNull
+    @Length(max = 10)
+    @Column(name = "nickname", length = 10, nullable = false)
     private String nickname;
 
-    @Column(name = "name", nullable = false)
+    @NotNull
+    @Length(max = 30)
+    @Column(name = "name", length = 30, nullable = false)
     private String name;
 
-    @Column(name = "phone", nullable = false)
+    @NotNull
+    @Length(max = 15)
+    @Column(name = "phone", length = 15, nullable = false)
     private String phone;
 
-    @Column(name = "birthday", nullable = false)
-    private String birthday;
+    @NotNull
+    @Column(name = "birthdate", nullable = false)
+    private LocalDate birthDate;
 
+    @NotNull
+    @Enumerated(EnumType.ORDINAL)
     @Column(name = "gender", nullable = false)
-    private char gender;
+    private Gender gender;
 
-    @Column(name = "created_at", nullable = false)
-    private LocalDateTime createdAt = LocalDateTime.now();
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Status status;
 
-    @Column(name = "updated_at", nullable = false)
-    private LocalDateTime updatedAt = LocalDateTime.now();
-
-    @Column(name = "status", nullable = false)
-    private String status;
+    @Builder
+    public User(final String id, final String password, final String nickname, final String name, final String phone,
+                final LocalDate birthDate, Gender gender) {
+        this.id = id;
+        this.password = password;
+        this.nickname = nickname;
+        this.name = name;
+        this.phone = phone;
+        this.birthDate = birthDate;
+        this.gender = gender;
+        this.status = Status.ACTIVATED;
+    }
 }
